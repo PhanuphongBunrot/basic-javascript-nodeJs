89b0395a5d857d005634649978eac78dd657ef38
nes120658 <neszaaalou@gmail.com>
2022-11-10 08:40:31 +0700

    10/11/65

--- sever.js
+++ sever.js
@@ -1,10 +1,20 @@
 const http = require('http')
 
 const server = http.createServer((req,res)=>{
-    res.write("<h1>Hello Nes</h1>")
+
+    const myhtml = `
+    <h1>Hello Nes</h1>
+    
+    `
+
+    res.write(myhtml)
     res.end()
 })
-let  post = 3000
-server.listen(post,()=>{
+let  post = 8080
+// server.listen(post,()=>{
+//     console.log(`start server in post ${post}` )
+// })
+
+server.listen(8080,'localhost',()=>{
     console.log(`start server in post ${post}` )
 })
